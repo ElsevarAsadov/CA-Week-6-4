337badf12e1f57c0ccfc64bb42ba93619e39e8b5
Esex <elseverclash57@gmail.com>
2023-10-29 16:06:00 +0400

    done

---
+++ CA-Week-6-4/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
+using System.Xml.Serialization;
+
+namespace CA_Week_6_4
+{
+    internal class Program
+    {
+        private static string _desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        static void Main(string[] args)
+        {
+            User user1 = new("Elsevar", "Asadov", "eli123");
+
+            //SerBinary(user);
+
+            //User u = DeSerBinary(Path.Combine(Program._desktopPath, "TestBinary.dat"));
+
+            //SerXML(user2);
+            //User ser u = DeSerXML(Path.Combine(Program._desktopPath, "TestXML.xml"));
+
+            SerJSON(user1);
+            User u = DeSerJSON(Path.Combine(Program._desktopPath, "TestJSON.json"));
+
+
+           //Console.WriteLine(u.Id);
+           //Console.WriteLine(u.Name);
+           //Console.WriteLine(u.Surname);
+        }
+
+        static void SerBinary(User user)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            
+            Stream stream = new FileStream(Path.Combine(Program._desktopPath, "TestBinary.dat"), FileMode.Create);
+
+            formatter.Serialize(stream, user);
+        }
+
+        static void SerXML(User user)
+        {
+            Stream stream = new FileStream(Path.Combine(Program._desktopPath, "TestXML.xml"), FileMode.Create);
+            XmlSerializer formatter = new XmlSerializer(typeof(User));
+
+            formatter.Serialize(stream, user);
+        }
+
+        static void SerJSON(User user)
+        {
+            
+            string json = JsonSerializer.Serialize(user);
+
+            File.WriteAllText(Path.Combine(Program._desktopPath, "TestJSON.json"), json);
+        }
+
+        static User? DeSerJSON(string path)
+        {
+
+            return JsonSerializer.Deserialize<User>(File.ReadAllText(path));
+        }
+
+        static User DeSerXML(string path)
+        {
+            Stream stream = new FileStream(path, FileMode.Open);
+
+            XmlSerializer formatter = new XmlSerializer(typeof(User));
+            User user = (User) formatter.Deserialize(stream);
+
+            return user;
+        }
+
+
+        static User DeSerBinary(string path)
+        {
+            Stream stream = new FileStream(path, FileMode.Open);
+
+            BinaryFormatter formatter = new BinaryFormatter();
+            User user = (User) formatter.Deserialize(stream);
+
+            return user;
+        }
+    }
+}
---
+++ CA-Week-6-4/User.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA_Week_6_4
+{
+    [Serializable]
+    public class User : ISerializable
+    {
+        private static int _id = 1;
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Username { get; set; }
+
+        public User(string name, string surname, string username)
+        {
+            Id = User._id;
+            Name = name;
+            Surname = surname;
+            Username = username;
+
+            User._id++;
+        }
+
+        public User()
+        {
+            
+        }
+
+        public User(SerializationInfo info, StreamingContext context)
+        {
+            Id = (int)info.GetValue("Id", typeof(int));
+            Name = (string)info.GetValue("Name", typeof(string));
+            Surname = (string)info.GetValue("Surname", typeof(string));
+            Username = (string)info.GetValue("Username", typeof(string));
+        }
+
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Id", Id, typeof(int));
+            info.AddValue("Name", Name, typeof(string));
+            info.AddValue("Surname", Surname, typeof(string));
+            info.AddValue("Username", Username, typeof(string));
+
+
+        }
+    }
+}
